45996a0c7d69e6cc1bcb9cc5456d87be6096fc8a
Gabriel Henrique de Oliveira <92988574+GabrielH1010@users.noreply.github.com>
2023-06-04 00:45:20 -0300

    Alteração na tela de agendamento
    Criação do autocomplete para buscar o profissional

--- animaclinics/src/components/Header/index.jsx
+++ animaclinics/src/components/Header/index.jsx
@@ -1,20 +1,29 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Logo, Title, ContentArea } from "./styles";
-import logo from "assets/logo.svg";
-import { useLocation, useHistory } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Logo, Title, ContentArea } from './styles';
+import logo from 'assets/logo.svg';
+import { useLocation, useHistory } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 const drawerWidth = 280;
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#BDD2E6",
+      main: '#BDD2E6',
     },
   },
 });
@@ -25,13 +34,23 @@ export default function PermanentDrawerLeft() {
   const path = location.pathname;
 
   const handleLogout = () => {
-    localStorage.removeItem("userData");
-    history.push("/");
+    Cookies.remove('userData');
+    history.push('/');
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex" }}>
+      <Box sx={{ display: 'flex' }}>
         <CssBaseline />
         <AppBar
           position="fixed"
@@ -41,16 +60,49 @@ export default function PermanentDrawerLeft() {
             ml: `${drawerWidth}px`,
           }}
         >
-          <Toolbar />
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
+            {/* <div></div>
+            <div>
+              <IconButton size="large" edge="end" color="inherit" aria-label="menu" onClick={handleClick}>
+                <Avatar>
+                  <AccountCircleIcon />
+                </Avatar>
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  <LockIcon sx={{ mr: 1 }} />
+                  Alterar senha
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                  <ExitToAppIcon sx={{ mr: 1 }} />
+                  Sair
+                </MenuItem>
+              </Menu>
+            </div> */}
+          </Toolbar>
         </AppBar>
         <Drawer
           sx={{
-            "& .MuiDrawer-paper": {
+            '& .MuiDrawer-paper': {
               width: drawerWidth,
-              justifyContent: "flex-start",
-              alignItems: "center",
-              backgroundColor: "#BDD2E6",
-              boxSizing: "border-box",
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              backgroundColor: '#BDD2E6',
+              boxSizing: 'border-box',
             },
           }}
           variant="permanent"
@@ -62,43 +114,25 @@ export default function PermanentDrawerLeft() {
               <h3>UnaClinics</h3>
             </Logo>
             <div className="navigation">
-              <Title
-                className={path === "/dashboard" && "select"}
-                onClick={() => history.push("/dashboard")}
-              >
+              <Title className={path === '/dashboard' && 'select'} onClick={() => history.push('/dashboard')}>
                 Dashboard
               </Title>
-              <Title
-                className={path === "/cadastro-paciente" && "select"}
-                onClick={() => history.push("/cadastro-paciente")}
-              >
+              <Title className={path === '/cadastro-paciente' && 'select'} onClick={() => history.push('/cadastro-paciente')}>
                 Cadastro paciente
               </Title>
-              <Title
-                className={path === "/anamnese" && "select"}
-                onClick={() => history.push("/anamnese")}
-              >
+              <Title className={path === '/anamnese' && 'select'} onClick={() => history.push('/anamnese')}>
                 Anamnese
               </Title>
-              <Title
-                className={path === "/agendamento" && "select"}
-                onClick={() => history.push("/agendamento")}
-              >
+              <Title className={path === '/agendamento' && 'select'} onClick={() => history.push('/agendamento')}>
                 Agendamento
               </Title>
-              <Title
-                className={path === "/" && "select"}
-                onClick={handleLogout}
-              >
+              <Title className={path === '/' && 'select'} onClick={handleLogout}>
                 Sair
               </Title>
             </div>
           </ContentArea>
         </Drawer>
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
-        >
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
           <Toolbar />
         </Box>
       </Box>
--- animaclinics/src/pages/Agendament/index.jsx
+++ animaclinics/src/pages/Agendament/index.jsx
@@ -5,11 +5,13 @@ import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import styled from 'styled-components';
 import { Container, ContentArea, ClaimOption, Title } from './styles';
-import { Header } from 'components';
+import { Header, Button } from 'components';
 import api from 'api';
 import { toast } from 'react-toastify';
 import Cookies from 'js-cookie';
 import { format } from 'date-fns';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const StyledCalendar = styled(FullCalendar)`
   flex: 1;
@@ -19,20 +21,31 @@ function Agendament() {
   const [userData, setUserData] = useState(null);
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedHours, setSelectedHours] = useState(null);
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+  const [doctor, setDoctor] = useState([]);
+  const [steps, setSteps] = useState(1);
   const calendarRef = useRef(null);
   const hour08 = useRef(null);
   const hour09 = useRef(null);
   const hour10 = useRef(null);
   const hour11 = useRef(null);
   const hour12 = useRef(null);
-  // const hour13 = useRef(null);
   const hour14 = useRef(null);
   const hour15 = useRef(null);
   const hour16 = useRef(null);
   const hour17 = useRef(null);
   const hour18 = useRef(null);
 
+  const fetchDoctor = async () => {
+    const resp = await api({
+      method: 'GET',
+      url: `/usuarios`,
+    });
+    setDoctor(resp.data);
+  };
+
   useEffect(() => {
+    fetchDoctor();
     const storedUserData = Cookies.get('userData');
     if (storedUserData) {
       try {
@@ -44,6 +57,42 @@ function Agendament() {
     } else {
       setUserData(null);
     }
+
+    const handleOutsideClick = (event) => {
+      if (
+        calendarRef.current &&
+        !calendarRef.current.contains(event.target) &&
+        hour08.current &&
+        !hour08.current.contains(event.target) &&
+        hour09.current &&
+        !hour09.current.contains(event.target) &&
+        hour10.current &&
+        !hour10.current.contains(event.target) &&
+        hour11.current &&
+        !hour11.current.contains(event.target) &&
+        hour12.current &&
+        !hour12.current.contains(event.target) &&
+        hour14.current &&
+        !hour14.current.contains(event.target) &&
+        hour15.current &&
+        !hour15.current.contains(event.target) &&
+        hour16.current &&
+        !hour16.current.contains(event.target) &&
+        hour17.current &&
+        !hour17.current.contains(event.target) &&
+        hour18.current &&
+        !hour18.current.contains(event.target)
+      ) {
+        setSelectedDate(null);
+        setSelectedHours(null);
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
   }, []);
 
   const handleAgendament = async () => {
@@ -55,13 +104,14 @@ function Agendament() {
         url: `/agendamentos`,
         data: {
           id_criador: userData.id,
-          id_medico: 21,
+          id_medico: selectedDoctor.id,
           data: formattedDate,
           hora: selectedHours,
         },
       });
       setSelectedDate(null);
       setSelectedHours(null);
+      setSteps(1);
       toast.success('Agendamento feito!', {
         position: toast.POSITION.TOP_RIGHT,
         theme: 'colored',
@@ -71,7 +121,6 @@ function Agendament() {
         position: toast.POSITION.TOP_RIGHT,
         theme: 'colored',
       });
-      // setLoading(false);
     }
   };
 
@@ -83,45 +132,9 @@ function Agendament() {
     setSelectedDate(info.start);
   };
 
-  const handleOutsideClick = (event) => {
-    if (
-      calendarRef.current &&
-      !calendarRef.current.contains(event.target) &&
-      hour08.current &&
-      !hour08.current.contains(event.target) &&
-      hour09.current &&
-      !hour09.current.contains(event.target) &&
-      hour10.current &&
-      !hour10.current.contains(event.target) &&
-      hour11.current &&
-      !hour11.current.contains(event.target) &&
-      hour12.current &&
-      !hour12.current.contains(event.target) &&
-      // hour13.current &&
-      // !hour13.current.contains(event.target) &&
-      hour14.current &&
-      !hour14.current.contains(event.target) &&
-      hour15.current &&
-      !hour15.current.contains(event.target) &&
-      hour16.current &&
-      !hour16.current.contains(event.target) &&
-      hour17.current &&
-      !hour17.current.contains(event.target) &&
-      hour18.current &&
-      !hour18.current.contains(event.target)
-    ) {
-      setSelectedDate(null);
-      setSelectedHours(null);
-    }
+  const goBack = () => {
+    if (steps > 1) setSteps(steps - 1);
   };
-
-  useEffect(() => {
-    document.addEventListener('click', handleOutsideClick);
-
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
-    };
-  }, []);
 
   const formatDate = (date) => {
     const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
@@ -132,143 +145,173 @@ function Agendament() {
     <>
       <Header />
       <Container>
-        <Title>
-          <h3>
-            Data do agendamento: <span>{selectedDate ? formatDate(selectedDate) : 'Sem data'}</span>
-          </h3>
-          <h3>
-            Horário do agendamento: <span>{selectedHours ? selectedHours : 'Sem hora'}</span>
-          </h3>
-        </Title>
-        <ContentArea>
-          <div className="container-calendar" ref={calendarRef}>
-            <StyledCalendar
-              schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
-              defaultView="dayGridMonth"
-              dateClick={handleDateClick}
-              displayEventTime={true}
-              header={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+        {steps === 1 && (
+          <>
+            <Title>
+              <h3>Selecione o profissional desejado com quem deseja realizar a consulta</h3>
+            </Title>
+            <Autocomplete
+              options={doctor}
+              sx={{ width: '100%' }}
+              style={{ margin: '20px 0 5px 0' }}
+              value={selectedDoctor}
+              getOptionLabel={(option) => option?.nome}
+              onChange={(event, newValue) => {
+                setSelectedDoctor(newValue);
               }}
-              selectable={true}
-              plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-              eventClick={(event) => {
-                console.log(event.event._def.publicId);
-              }}
-              select={handleSelectedDates}
-              eventLimit={1}
-              date={selectedDate}
-              onChange={handleSelectedDates}
-              weekends={false}
-              dayRender={(info) => {
-                const weekendDay = info.date.getDay() === 0 || info.date.getDay() === 6;
-                const pastDate = selectedDate !== null && info.date < selectedDate;
-                const futureDate = selectedDate !== null && info.date > selectedDate;
-                if (weekendDay || pastDate || futureDate) {
-                  info.day.el.classList.add('disabled-day');
-                }
-              }}
+              renderOption={(props, option) => (
+                <>
+                  <p style={{ paddingLeft: 10, cursor: 'pointer' }} {...props} className="title">
+                    {option?.nome} - {option?.profissao}
+                  </p>
+                </>
+              )}
+              renderInput={(params) => <TextField {...params} label="Selecione o parceiro" placeholder="Digite para buscar..." />}
             />
-          </div>
-          {selectedDate !== null && (
-            <div className="options-area">
-              <ClaimOption
-                selected={selectedHours === '08:00'}
-                onClick={() => {
-                  setSelectedHours('08:00');
-                }}
-                ref={hour08}
-              >
-                08:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '09:00'}
-                onClick={() => {
-                  setSelectedHours('09:00');
-                }}
-                ref={hour09}
-              >
-                09:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '10:00'}
-                onClick={() => {
-                  setSelectedHours('10:00');
-                }}
-                ref={hour10}
-              >
-                10:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '11:00'}
-                onClick={() => {
-                  setSelectedHours('11:00');
-                }}
-                ref={hour11}
-              >
-                11:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '12:00'}
-                onClick={() => {
-                  setSelectedHours('12:00');
-                }}
-                ref={hour12}
-              >
-                12:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '14:00'}
-                onClick={() => {
-                  setSelectedHours('14:00');
-                }}
-                ref={hour14}
-              >
-                14:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '15:00'}
-                onClick={() => {
-                  setSelectedHours('15:00');
-                }}
-                ref={hour15}
-              >
-                15:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '16:00'}
-                onClick={() => {
-                  setSelectedHours('16:00');
-                }}
-                ref={hour16}
-              >
-                16:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '17:00'}
-                onClick={() => {
-                  setSelectedHours('17:00');
-                }}
-                ref={hour17}
-              >
-                17:00
-              </ClaimOption>
-              <ClaimOption
-                selected={selectedHours === '18:00'}
-                onClick={() => {
-                  setSelectedHours('18:00');
-                }}
-                ref={hour18}
-              >
-                18:00
-              </ClaimOption>
-            </div>
-          )}
+            {selectedDoctor && <Button onClick={() => setSteps(2)}>Prosseguir</Button>}
+          </>
+        )}
+        {steps === 2 && (
+          <>
+            <Title>
+              <h3>
+                Data do agendamento: <span>{selectedDate ? formatDate(selectedDate) : 'Sem data'}</span>
+              </h3>
+              <h3>
+                Horário do agendamento: <span>{selectedHours ? selectedHours : 'Sem hora'}</span>
+              </h3>
+            </Title>
+            <ContentArea>
+              <div className="container-calendar" ref={calendarRef}>
+                <StyledCalendar
+                  schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+                  defaultView="dayGridMonth"
+                  dateClick={handleDateClick}
+                  displayEventTime={true}
+                  header={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+                  }}
+                  selectable={true}
+                  plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                  eventClick={(event) => {
+                    console.log(event.event._def.publicId);
+                  }}
+                  select={handleSelectedDates}
+                  eventLimit={1}
+                  date={selectedDate}
+                  onChange={handleSelectedDates}
+                  weekends={false}
+                  dayRender={(info) => {
+                    const weekendDay = info.date.getDay() === 0 || info.date.getDay() === 6;
+                    const pastDate = selectedDate !== null && info.date < selectedDate;
+                    const futureDate = selectedDate !== null && info.date > selectedDate;
+                    if (weekendDay || pastDate || futureDate) {
+                      info.day.el.classList.add('disabled-day');
+                    }
+                  }}
+                />
+              </div>
+              {selectedDate !== null && (
+                <div className="options-area">
+                  <ClaimOption
+                    selected={selectedHours === '08:00'}
+                    onClick={() => {
+                      setSelectedHours('08:00');
+                    }}
+                    ref={hour08}
+                  >
+                    08:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '09:00'}
+                    onClick={() => {
+                      setSelectedHours('09:00');
+                    }}
+                    ref={hour09}
+                  >
+                    09:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '10:00'}
+                    onClick={() => {
+                      setSelectedHours('10:00');
+                    }}
+                    ref={hour10}
+                  >
+                    10:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '11:00'}
+                    onClick={() => {
+                      setSelectedHours('11:00');
+                    }}
+                    ref={hour11}
+                  >
+                    11:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '12:00'}
+                    onClick={() => {
+                      setSelectedHours('12:00');
+                    }}
+                    ref={hour12}
+                  >
+                    12:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '14:00'}
+                    onClick={() => {
+                      setSelectedHours('14:00');
+                    }}
+                    ref={hour14}
+                  >
+                    14:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '15:00'}
+                    onClick={() => {
+                      setSelectedHours('15:00');
+                    }}
+                    ref={hour15}
+                  >
+                    15:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '16:00'}
+                    onClick={() => {
+                      setSelectedHours('16:00');
+                    }}
+                    ref={hour16}
+                  >
+                    16:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '17:00'}
+                    onClick={() => {
+                      setSelectedHours('17:00');
+                    }}
+                    ref={hour17}
+                  >
+                    17:00
+                  </ClaimOption>
+                  <ClaimOption
+                    selected={selectedHours === '18:00'}
+                    onClick={() => {
+                      setSelectedHours('18:00');
+                    }}
+                    ref={hour18}
+                  >
+                    18:00
+                  </ClaimOption>
+                </div>
+              )}
 
-          <button onClick={handleAgendament}>Marcar agendamento</button>
-        </ContentArea>
+              <button onClick={handleAgendament}>Marcar agendamento</button>
+            </ContentArea>
+          </>
+        )}
       </Container>
     </>
   );
